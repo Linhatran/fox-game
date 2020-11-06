9cefbe3dde3a965944036171aedf0efd8b49fd00
Linh Tran <linhtanl51@gmail.com>
2020-11-06 09:49:41 -0600

    everything works

--- src/init.js
+++ src/init.js
@@ -1,8 +1,8 @@
-const TICK_RATE = 1000;
+const TICK_RATE = 2000;
 const ICONS = ["fish", "poop", "weather"];
-const RAIN_CHANCE = 0.6;
+const RAIN_CHANCE = 0.3;
 const SCENES = ["day", "rain"];
-const DAY_LENGTH = 20;
+const DAY_LENGTH = 40;
 const NIGHT_LENGTH = 3;
 
 const modFox = (state) =>
@@ -14,13 +14,13 @@ const writeModal = (text = "") => {
     ".modal"
   ).innerHTML = `<div class="modal-inner">${text}</div>`;
 };
-const getNextHungryTime = (clock) => Math.floor(Math.random() * 3) + 5 + clock;
-const getNextPoopTime = (clock) => Math.floor(Math.random() * 2) + 3 + clock;
-const getDieTime = (clock) => Math.floor(Math.random() * 3) + 5 + clock;
+const getNextHungryTime = (clock) => Math.floor(Math.random() * 3) + 4 + clock;
+const getNextPoopTime = (clock) => Math.floor(Math.random() * 3) + 4 + clock;
+const getDieTime = (clock) => Math.floor(Math.random() * 3) + 4 + clock;
 
 const gameState = {
   current: "INIT",
-  clock: 0,
+  clock: -1,
   wakeTime: -1,
   hungryTime: -1,
   sleepTime: -1,
@@ -54,7 +54,7 @@ const gameState = {
   },
   tick() {
     this.clock++;
-    console.log(`clock ${this.clock}`);
+    console.log(`clock ${this.clock} ${this.current}`);
     if (this.clock === this.wakeTime) {
       this.wake();
     } else if (this.clock === this.hungryTime) {
@@ -74,37 +74,31 @@ const gameState = {
   },
   startGame() {
     this.current = "HATCHING";
-    console.log("Fox is hatching");
     modFox("egg");
     modScene("day");
     writeModal();
     this.wakeTime = this.clock + 3;
-    console.log("Fox wakes at ", this.wakeTime);
   },
   wake() {
     this.current = "IDLING";
     this.wakeTime = -1;
     this.hungryTime = getNextHungryTime(this.clock);
-    console.log("Fox gets hungry at", this.hungryTime);
     modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
     this.foxDirection();
     this.sleepTime = this.clock + DAY_LENGTH;
-    console.log("Fox sleeps at", this.sleepTime);
   },
   getHungry() {
     this.current = "HUNGRY";
     this.hungryTime = -1;
     this.dieTime = getDieTime(this.clock);
-    console.log("Fox dies because of no fish at", this.dieTime);
     modFox("hungry");
   },
   sleep() {
     this.current = "SLEEPING";
     this.clearTime();
     this.wakeTime = this.clock + NIGHT_LENGTH;
-    console.log("Fox is sleeping, will wake at", this.wakeTime);
     modFox("sleep");
     modScene("night");
   },
@@ -122,32 +116,24 @@ const gameState = {
   },
   feed() {
     if (this.current !== "HUNGRY") return;
-    modFox("eating");
     this.current = "FEEDING";
-    console.log("Feeding fox");
-    this.hungryTime = -1;
     this.timeToStartCelebrating = this.clock + 2;
-    console.log("Fox celebrate at", this.timeToStartCelebrating);
     this.dieTime = -1;
     this.poopTime = getNextPoopTime(this.clock);
-    console.log("Fox poops at", this.poopTime);
+    modFox("eating");
   },
   poop() {
     this.current = "POOPING";
     modFox("pooping");
-    console.log(this.current);
     this.poopTime = -1;
     this.dieTime = getDieTime(this.clock);
-    console.log("Fox dies because of poop at", this.dieTime);
   },
   cleanPoop() {
     if (this.current === "POOPING") {
       this.dieTime = -1;
-      console.log("fox die time is", this.dieTime);
       togglePoopBag(true);
       this.startCelebrating();
       this.hungryTime = getNextHungryTime(this.clock);
-      console.log("cleaning poop");
     }
   },
   changeWeather() {
