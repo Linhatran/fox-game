9dde8e0cdf50c119fdfa5748443ad917a95a75c9
Linh Tran <linhtanl51@gmail.com>
2020-11-06 00:09:51 -0600

    still need to fix poop and die

--- src/init.js
+++ src/init.js
@@ -9,10 +9,14 @@ const modFox = (state) =>
   (document.querySelector(".fox").className = `fox fox-${state}`);
 const modScene = (state) =>
   (document.querySelector(".game").className = `game ${state}`);
-
-const timeToGetHungry = (clock) => Math.floor(Math.random() * 4) + 5 + clock; //5-8s
-const timeToPoop = (clock) => Math.floor(Math.random() * 3) + 3 + clock; //3-5s
-const timeToDie = (clock) => Math.floor(Math.random() * 5) + 4 + clock; //4-8s
+const writeModal = (text = "") => {
+  document.querySelector(
+    ".modal"
+  ).innerHTML = `<div class="modal-inner">${text}</div>`;
+};
+const getNextHungryTime = (clock) => Math.floor(Math.random() * 3) + 5 + clock;
+const getNextPoopTime = (clock) => Math.floor(Math.random() * 2) + 3 + clock;
+const getDieTime = (clock) => Math.floor(Math.random() * 3) + 5 + clock;
 
 const gameState = {
   current: "INIT",
@@ -20,10 +24,32 @@ const gameState = {
   wakeTime: -1,
   hungryTime: -1,
   sleepTime: -1,
+  poopTime: -1,
+  dieTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   scene: 0,
   handleUserAction(icon) {
-    if (this.current === "INIT") {
+    if (
+      ["FEEDING", "HATCHING", "SLEEPING", "CELEBRATING"].includes(this.current)
+    ) {
+      return;
+    }
+
+    if (this.current === "INIT" || this.current === "DEAD") {
       this.startGame();
+      return;
+    }
+    switch (icon) {
+      case "fish":
+        this.feed();
+        break;
+      case "poop":
+        this.cleanPoop();
+        break;
+      case "weather":
+        this.changeWeather();
+        break;
     }
   },
   tick() {
@@ -35,6 +61,14 @@ const gameState = {
       this.getHungry();
     } else if (this.clock === this.sleepTime) {
       this.sleep();
+    } else if (this.clock === this.timeToStartCelebrating) {
+      this.startCelebrating();
+    } else if (this.clock === this.timeToEndCelebrating) {
+      this.endCelebrating();
+    } else if (this.clock === this.poopTime) {
+      this.poop();
+    } else if (this.clock === this.dieTime) {
+      this.die();
     }
     return this.clock;
   },
@@ -42,41 +76,106 @@ const gameState = {
     this.current = "HATCHING";
     console.log("Fox is hatching");
     modFox("egg");
+    modScene("day");
+    writeModal();
     this.wakeTime = this.clock + 3;
     console.log("Fox wakes at ", this.wakeTime);
   },
   wake() {
-    this.wakeTime = -1;
     this.current = "IDLING";
-    console.log("Fox is idling");
+    this.wakeTime = -1;
+    this.hungryTime = getNextHungryTime(this.clock);
+    console.log("Fox gets hungry at", this.hungryTime);
     modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
-    this.foxDirection(SCENES[this.scene]);
-    this.hungryTime = timeToGetHungry(this.clock);
-    console.log("Fox gets hungry at ", this.hungryTime);
+    this.foxDirection();
     this.sleepTime = this.clock + DAY_LENGTH;
     console.log("Fox sleeps at", this.sleepTime);
   },
   getHungry() {
-    this.hungryTime = -1;
     this.current = "HUNGRY";
+    this.hungryTime = -1;
+    this.dieTime = getDieTime(this.clock);
+    console.log("Fox dies because of no fish at", this.dieTime);
     modFox("hungry");
   },
   sleep() {
-    this.sleepTime = -1;
     this.current = "SLEEPING";
+    this.clearTime();
     this.wakeTime = this.clock + NIGHT_LENGTH;
-    console.log("Fox is sleeping");
-    console.log("Fox wakes at", this.wakeTime);
+    console.log("Fox is sleeping, will wake at", this.wakeTime);
     modFox("sleep");
     modScene("night");
   },
-  foxDirection(scene) {
+  startCelebrating() {
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.current = "IDLING";
+    this.timeToEndCelebrating = -1;
+    this.foxDirection();
+    togglePoopBag(false); //this will make poop bag appear
+  },
+  feed() {
+    if (this.current !== "HUNGRY") return;
+    modFox("eating");
+    this.current = "FEEDING";
+    console.log("Feeding fox");
+    this.hungryTime = -1;
+    this.timeToStartCelebrating = this.clock + 2;
+    console.log("Fox celebrate at", this.timeToStartCelebrating);
+    this.dieTime = -1;
+    this.poopTime = getNextPoopTime(this.clock);
+    console.log("Fox poops at", this.poopTime);
+  },
+  poop() {
+    this.current = "POOPING";
+    modFox("pooping");
+    console.log(this.current);
+    this.poopTime = -1;
+    this.dieTime = getDieTime(this.clock);
+    console.log("Fox dies because of poop at", this.dieTime);
+  },
+  cleanPoop() {
+    if (this.current === "POOPING") {
+      this.dieTime = -1;
+      console.log("fox die time is", this.dieTime);
+      togglePoopBag(true);
+      this.startCelebrating();
+      this.hungryTime = getNextHungryTime(this.clock);
+      console.log("cleaning poop");
+    }
+  },
+  changeWeather() {
+    this.scene = (this.scene + 1) % SCENES.length;
+    modScene(SCENES[this.scene]);
+    this.foxDirection();
+  },
+  die() {
+    this.current = "DEAD";
+    this.clearTime();
+    modFox("dead");
+    modScene("dead");
+    writeModal("Fox has died :( <br/>Click the middle button to restart!");
+  },
+  clearTime() {
+    this.wakeTime = -1;
+    this.hungryTime = -1;
+    this.sleepTime = -1;
+    this.poopTime = -1;
+    this.dieTime = -1;
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = -1;
+  },
+  foxDirection() {
     if (this.current === "IDLING") {
-      if (scene === "rain") {
+      if (SCENES[this.scene] === "rain") {
         modFox("rain");
-      } else if (scene === "day") {
+      } else {
         modFox("idling");
       }
     }
@@ -89,7 +188,8 @@ const toggleHighlighted = (iconIndex, isShown) => {
     .querySelector(`.${ICONS[iconIndex]}-icon`)
     .classList.toggle("highlighted", isShown);
 };
-
+const togglePoopBag = (isShown) =>
+  document.querySelector(".poop-bag").classList.toggle("hidden", !isShown);
 //write a function to loop over buttons
 function initButtons(handleUserAction) {
   let iconIndex = 0;
