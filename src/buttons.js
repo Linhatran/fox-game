3c21bcaf770f212a9c97853ea0d1777a39f9e8f1
Linh Tran <linhtanl51@gmail.com>
2020-11-07 11:25:16 -0600

    changed to rem and added button keyup eventlistener

--- src/buttons.js
+++ src/buttons.js
@@ -9,18 +9,37 @@ const toggleHighlighted = (iconIndex, isShown) => {
 export default function initButtons(handleUserAction) {
   let iconIndex = 0;
   function buttonClick(e) {
-    if (e.target.classList.value.includes("right-btn")) {
+    if (
+      e.target.classList.value.includes("right-btn") ||
+      e.key === "ArrowRight"
+    ) {
       toggleHighlighted(iconIndex, false);
       iconIndex = (iconIndex + 1) % ICONS.length;
+      document.querySelector(".right-btn").classList.add("btn-key-hover");
+      document.querySelector(".left-btn").classList.remove("btn-key-hover");
+      document.querySelector(".middle-btn").classList.remove("btn-key-hover");
       toggleHighlighted(iconIndex, true);
-    } else if (e.target.classList.value.includes("left-btn")) {
+    } else if (
+      e.target.classList.value.includes("left-btn") ||
+      e.key === "ArrowLeft"
+    ) {
       toggleHighlighted(iconIndex, false);
       iconIndex = (iconIndex + 2) % ICONS.length;
+      document.querySelector(".left-btn").classList.add("btn-key-hover");
+      document.querySelector(".right-btn").classList.remove("btn-key-hover");
+      document.querySelector(".middle-btn").classList.remove("btn-key-hover");
       toggleHighlighted(iconIndex, true);
-    } else {
+    } else if (
+      e.target.classList.value.includes("middle-btn") ||
+      e.key === "Enter"
+    ) {
       //middle btn is clicked
       handleUserAction(ICONS[iconIndex]);
+      document.querySelector(".middle-btn").classList.add("btn-key-hover");
+      document.querySelector(".right-btn").classList.remove("btn-key-hover");
+      document.querySelector(".left-btn").classList.remove("btn-key-hover");
     }
   }
   document.querySelector(".buttons").addEventListener("click", buttonClick);
+  document.addEventListener("keyup", buttonClick);
 }
